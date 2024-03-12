8b7d9dfe675f5dd5105533ab3194cffc32676300
chiragjain307 <chiragjain.jain7@gmail.com>
2024-03-12 18:27:38 +0530

    responsive

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -34,7 +34,7 @@ function Home() {
                                 Our Latest Posts
                             </h1>
                         </div>
-                        <div className='flex justify-center gap-5 mt-8 sm:flex-wrap '>
+                        <div className='flex justify-center gap-5 mt-8 flex-wrap '>
                             {posts.map((post) => (
                                 <div key={post.$id} className='p-2 w-full sm:w-1/2 md:w-1/3 xl:w-1/4'>
                                     <PostCard {...post} />
